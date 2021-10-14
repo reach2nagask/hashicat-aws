74adf6023024b78a1fbab45e43609a155be17865
root <root@p-rve1ayxcppxh-workstation.c.instruqt-prod.internal>
2021-10-14 11:26:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/MyHandle/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix  = var.prefix
+  #bucket_prefix  = var.prefix
+  bucket = "skBucket"
 }
